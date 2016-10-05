9619789ef33f036d2b3af7bb2fa1ee676b55fa5d
Dmitriy Tarasov <dm.vl.tarasov@gmail.com>
2016-10-05 16:55:46 +0300

    Initial commit - basic app without dagger

---
+++ app/src/main/java/today/tarasov/activityscope/SharedFragment.java
@@ -0,0 +1,21 @@
+package today.tarasov.activityscope;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * @author Dmitriy Tarasov
+ */
+
+public class SharedFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_shared, container, false);
+    }
+}
